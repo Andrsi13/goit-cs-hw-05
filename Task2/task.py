e94324a5c4f9fb90ea7345bd74a756048911c379
Andrsi13 <vladika.andrsi@gmail.com>
2024-08-03 17:05:20 +0300

    task 2

---
+++ Task2/task.py
@@ -0,0 +1,76 @@
+import string
+import requests
+import matplotlib.pyplot as plt
+from concurrent.futures import ThreadPoolExecutor
+from collections import defaultdict, Counter
+
+def get_text(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Перевірка на помилки HTTP
+        return response.text
+    except requests.RequestException as e:
+        print(f"Error fetching the text: {e}")
+        return None
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans("", "", string.punctuation))
+
+def map_function(word):
+    return word, 1
+
+def shuffle_function(mapped_values):
+    shuffled = defaultdict(list)
+    for key, value in mapped_values:
+        shuffled[key].append(value)
+    return shuffled.items()
+
+def reduce_function(key_values):
+    key, values = key_values
+    return key, sum(values)
+
+def map_reduce(text):
+    # Видалення знаків пунктуації
+    text = remove_punctuation(text)
+    words = text.split()
+
+    # Паралельний Мапінг
+    with ThreadPoolExecutor() as executor:
+        mapped_values = list(executor.map(map_function, words))
+
+    # Крок 2: Shuffle
+    shuffled_values = shuffle_function(mapped_values)
+
+    # Паралельна Редукція
+    with ThreadPoolExecutor() as executor:
+        reduced_values = list(executor.map(reduce_function, shuffled_values))
+
+    return dict(reduced_values)
+
+def visualize_top_words(word_counts, top_n=10):
+    # Сортування слів за частотою
+    sorted_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+    top_words = sorted_words[:top_n]
+    words, counts = zip(*top_words) if top_words else ([], [])
+
+    plt.figure(figsize=(12, 8))
+    plt.barh(words, counts, color='skyblue')
+    plt.xlabel('Frequency')
+    plt.ylabel('Words')
+    plt.title('Top Words Frequency')
+    plt.gca().invert_yaxis()  # Інвертувати вісь Y, щоб топ-слова були зверху
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    # Вхідний текст для обробки
+    url = "https://gutenberg.net.au/ebooks01/0100021.txt"  # Замість цього вставте URL-адресу
+    text = get_text(url)
+    if text:
+        # Виконання MapReduce на вхідному тексті
+        word_counts = map_reduce(text)
+
+        # Візуалізація топ слів
+        visualize_top_words(word_counts)
+    else:
+        print("Помилка: Не вдалося отримати вхідний текст.")
